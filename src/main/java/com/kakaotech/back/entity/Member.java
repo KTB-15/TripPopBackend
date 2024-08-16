5f7a6cef15c9bc853ec8f0811eb9317fe8a8310b
teddy.kim <qazkyj0310@gmail.com>
2024-08-16 11:09:59 +0900

    add: line space

--- src/main/java/com/kakaotech/back/entity/Member.java
+++ src/main/java/com/kakaotech/back/entity/Member.java
@@ -19,33 +19,49 @@ import java.time.LocalDateTime;
 public class Member {
     @Id
     private String id;
+
     @Column(name = "member_id", unique = true)
     private String memberId;
+
     private String password;
+
     private String nickname;
     private String gender;
+
     private Integer age;
+
     @Column(name = "travel_like_sido")
     private String travelLikeSIDO;
+
     @Column(name = "travel_like_sgg")
     private String travelLikeSGG;
+
     @Column(name = "travel_style_1")
     private String travelStyle1;
+
     @Column(name = "travel_style_2")
     private String travelStyle2;
+
     @Column(name = "travel_style_3")
     private String travelStyle3;
+
     @Column(name = "travel_style_4")
     private String travelStyle4;
+
     @Column(name = "travel_style_5")
     private String travelStyle5;
+
     @Column(name = "travel_style_6")
     private String travelStyle6;
+
     @Column(name = "travel_style_7")
     private String travelStyle7;
+
     @Column(name = "travel_style_8")
     private String travelStyle8;
+
     @CreatedDate
+    @Column(columnDefinition = "TIMESTAMP")
     private LocalDateTime registerAt;
 
     public void SetTravelStyle1(String travelStyle1) {

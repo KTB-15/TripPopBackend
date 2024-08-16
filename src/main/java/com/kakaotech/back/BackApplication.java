97073f8d3601b4cb86684ea59994c8b6ed4ce672
Sin Han <vrdhan212@gmail.com>
2024-08-16 11:21:40 +0900

    refactor: separate jpa audit config

--- src/main/java/com/kakaotech/back/BackApplication.java
+++ src/main/java/com/kakaotech/back/BackApplication.java
@@ -2,10 +2,9 @@ package com.kakaotech.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
-@EnableJpaAuditing //
 public class BackApplication {
 
 	public static void main(String[] args) {

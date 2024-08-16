93863eeedbf1cca4c815757921ba87954f10da5b
teddy.kim <qazkyj0310@gmail.com>
2024-08-16 16:56:15 +0900

    add: relationship member and history test

--- src/test/java/com/kakaotech/back/repository/MemberRepositoryTest.java
+++ src/test/java/com/kakaotech/back/repository/MemberRepositoryTest.java
@@ -1,14 +1,16 @@
 package com.kakaotech.back.repository;
 
 import com.kakaotech.back.entity.Gender;
+import com.kakaotech.back.entity.History;
 import com.kakaotech.back.entity.Member;
 import com.kakaotech.back.entity.SGG;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -21,12 +23,21 @@ public class MemberRepositoryTest {
     private MemberRepository memberRepository;
     @Autowired
     private SGGRepository sggRepository;
+    @Autowired
+    private HistoryRepository historyRepository;
     private Member member;
+    private History history;
 
     @BeforeEach
     public void setUp() {
         SGG sgg = new SGG("sgg1", "sidoCode1", "sggCode1", "sidoName1", "sggName1");
         sggRepository.save(sgg);  // SGG 엔터티를 먼저 저장
+
+
+        history = History.builder()
+                .content("Sample content")
+                .build();
+        history = historyRepository.save(history);
 
         member = Member.builder()
                 .id("member1")
@@ -35,6 +46,7 @@ public class MemberRepositoryTest {
                 .nickname("nickname")
                 .gender(Gender.MALE)
                 .age(30)
+                .histories(List.of(history))
                 .sgg(sgg)
                 .build();
     }
@@ -52,5 +64,12 @@ public class MemberRepositoryTest {
         Member foundMember = memberRepository.findById("member1").orElse(null);
         assertNotNull(foundMember);
         assertEquals("mem001", foundMember.getMemberId());
+    }
+
+    @Test
+    public void testRelationshipMemberAndHistory() {
+        memberRepository.save(member);
+        Member foundMember = memberRepository.findById("member1").orElse(null);
+        assertEquals("Sample content", foundMember.getHistories().get(0).getContent());
     }
 }

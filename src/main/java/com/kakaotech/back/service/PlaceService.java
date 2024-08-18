bed40e18bb6404c1f0dae6b66c4fbcc744a889a5
Sin Han <vrdhan212@gmail.com>
2024-08-18 23:27:29 +0900

    add: place service

---
+++ src/main/java/com/kakaotech/back/service/PlaceService.java
@@ -0,0 +1,19 @@
+package com.kakaotech.back.service;
+
+import com.kakaotech.back.common.exception.ErrorMessage;
+import com.kakaotech.back.common.exception.PlaceException;
+import com.kakaotech.back.entity.segment.PlaceCoord;
+import com.kakaotech.back.repository.PlaceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PlaceService {
+    private final PlaceRepository placeRepository;
+
+    public PlaceCoord getCoordinate(String placeId) {
+        if (!placeRepository.existsById(placeId)) throw new PlaceException(ErrorMessage.PLACE_NOT_FOUND);
+        return placeRepository.findCoordById(placeId).get();
+    }
+}

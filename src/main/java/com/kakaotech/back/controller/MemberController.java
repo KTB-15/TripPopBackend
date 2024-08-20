2a4f509337efc0d59b5c657b45d01a17b96bebf9
teddy.kim <qazkyj0310@gmail.com>
2024-08-21 00:24:54 +0900

    add: save member

--- src/main/java/com/kakaotech/back/controller/MemberController.java
+++ src/main/java/com/kakaotech/back/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.kakaotech.back.controller;
 
+import com.kakaotech.back.dto.RegisterMemberDto;
 import com.kakaotech.back.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,5 +21,15 @@ public class MemberController {
         Map<String, Boolean> response = new HashMap<>();
         response.put("exists", exists);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> registerMember(@RequestBody RegisterMemberDto registerMemberDto) {
+        try {
+            memberService.saveMember(registerMemberDto);
+            return ResponseEntity.ok("회원가입이 완료되었습니다.");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }

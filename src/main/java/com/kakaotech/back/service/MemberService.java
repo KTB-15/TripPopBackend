cac50f86885456d0abe19c5c61944e5a195f5554
teddy.kim <qazkyj0310@gmail.com>
2024-08-21 00:26:16 +0900

    add: save member

--- src/main/java/com/kakaotech/back/service/MemberService.java
+++ src/main/java/com/kakaotech/back/service/MemberService.java
@@ -1,8 +1,11 @@
 package com.kakaotech.back.service;
 
+import com.kakaotech.back.dto.RegisterMemberDto;
 import com.kakaotech.back.entity.Member;
+import com.kakaotech.back.entity.SGG;
 import com.kakaotech.back.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,8 +16,34 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public boolean existsByMemberId(String memberId) {
         return memberRepository.existsByMemberId(memberId);
+    }
+
+    public Member saveMember(RegisterMemberDto registerMemberDto) {
+        if (memberRepository.existsByMemberId(registerMemberDto.getId())) {
+            throw new RuntimeException("이미 존재하는 아이디입니다.");
+        }
+
+        // TODO
+        // ssg id값 어떻게 가져옴?
+        // age
+        // gender 수정
+        SGG sgg = SGG.builder()
+                .sidoName(registerMemberDto.getArea())
+                .sggName(registerMemberDto.getSubArea())
+                .build();
+
+        Member member = Member.builder()
+                .memberId(registerMemberDto.getId())
+                .password(passwordEncoder.encode(registerMemberDto.getPassword()))
+                .gender(registerMemberDto.getGender())
+                .age(registerMemberDto.getAgeGroup())
+                .sgg(sgg)
+                .build();
+
+        return memberRepository.save(member);
     }
 }

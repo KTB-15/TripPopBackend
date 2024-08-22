39d0e0e621cb9cecf2bf95f317cf2b78af230a99
Sin Han <vrdhan212@gmail.com>
2024-08-22 22:10:29 +0900

    update: get nearby place from google using coordinate

--- src/main/java/com/kakaotech/back/service/PlaceService.java
+++ src/main/java/com/kakaotech/back/service/PlaceService.java
@@ -5,19 +5,64 @@ import com.kakaotech.back.common.exception.PlaceException;
 import com.kakaotech.back.entity.projection.PlaceCoordVO;
 import com.kakaotech.back.repository.PlaceRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClient;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PlaceService {
     private final PlaceRepository placeRepository;
+    private final RestClient restClient;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final String NEARBY_SEARCH_URL = "https://places.googleapis.com/v1/places:searchNearby"; // Google Places API URL
 
     // 좌표만 조회
-    public PlaceCoordVO getCoordinate(Long placeId) {
+    PlaceCoordVO getCoordinate(Long placeId) {
         return placeRepository
                 .findCoordById(placeId)
                 .orElseThrow(() -> new PlaceException(ErrorMessage.PLACE_NOT_FOUND));
     }
 
+    // 좌표를 통해 구글 Place 주변 장소 검색
+    String getNearbyPlace(PlaceCoordVO coord) {
+        var body = getBody(coord);
 
+        try {
+            return restClient.post()
+                    .uri(NEARBY_SEARCH_URL)
+                    .body(body)
+                    .retrieve()
+                    .body(String.class);
+        } catch (HttpClientErrorException e) {
+            logger.error("FAIL TO GET GOOGLE PLACE INFO: {} - {}",
+                    e.getStatusCode().value(),
+                    e.getResponseBodyAsString());
+            throw e;
+        }
+    }
+
+    private static Map<String, Object> getBody(PlaceCoordVO coord) {
+        Double latitude = coord.getxCoord();
+        Double longitude = coord.getyCoord();
+        return Map.of(
+                "maxResultCount", 3,
+                "rankPreference", "DISTANCE",
+                "locationRestriction", Map.of(
+                        "circle", Map.of(
+                                "center", Map.of(
+                                        "latitude", latitude,
+                                        "longitude", longitude
+                                ),
+                                "radius", 100
+                        )
+                )
+        );
+    }
 }

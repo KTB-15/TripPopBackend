10db7ffcc61dd81a0ba796aae27a046dcc5631e3
Sin Han <vrdhan212@gmail.com>
2024-08-22 22:03:32 +0900

    update: add custom error code

--- src/main/java/com/kakaotech/back/common/exception/ErrorMessage.java
+++ src/main/java/com/kakaotech/back/common/exception/ErrorMessage.java
@@ -1,7 +1,6 @@
 package com.kakaotech.back.common.exception;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
@@ -11,9 +10,10 @@ public enum ErrorMessage {
     // 404
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "NO USER"),
     PLACE_NOT_FOUND(HttpStatus.NOT_FOUND, "NO PLACE"),
-    FAVOURITE_NOT_FOUND(HttpStatus.NOT_FOUND, "NO FAVOURITE");
+    FAVOURITE_NOT_FOUND(HttpStatus.NOT_FOUND, "NO FAVOURITE"),
 
-
+    // 502
+    GET_PLACE_IMAGE_FAILED(HttpStatus.BAD_GATEWAY, "FAILED TO GET PLACE IMAGE FROM GOOGLE");
     private final HttpStatus httpStatus;
     private final String message;
 }

6fbaf3639639730bc49532055a3fb96aae64e251
Sin Han <vrdhan212@gmail.com>
2024-08-23 23:47:16 +0900

    refactor: change custom exception

--- src/main/java/com/kakaotech/back/service/PlaceService.java
+++ src/main/java/com/kakaotech/back/service/PlaceService.java
@@ -1,8 +1,10 @@
 package com.kakaotech.back.service;
 
+import com.kakaotech.back.common.exception.GoogleApiException;
 import com.kakaotech.back.common.exception.NotFoundException;
-import com.kakaotech.back.entity.projection.PlaceCoordVO;
+import com.kakaotech.back.vo.PlaceCoordVO;
 import com.kakaotech.back.repository.PlaceRepository;
+import com.kakaotech.back.vo.GooglePlaceIdVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
@@ -24,43 +26,42 @@ public class PlaceService {
     private final String NEARBY_SEARCH_URL = "https://places.googleapis.com/v1/places:searchNearby"; // 좌표의 Place Id 조회
     private final String PLACE_REFERENCE_URL = "https://places.googleapis.com/v1/places"; // Place Id에 대한 정보 조회
 
-    public PlaceCoordVO getCoordinate(Long placeId) {
-        if (!placeRepository.existsById(placeId)) throw new NotFoundException(placeId+"로 생성된 place는 존재하지 않습니다.");
-        return placeRepository.findCoordById(placeId).get();
     // 좌표를 통해 구글 Place 주변 장소 검색
     public GooglePlaceIdVO getNearbyPlace(Long placeId) {
         // 존재하는 id인지 확인
         PlaceCoordVO coord = getCoordinate(placeId);
         var body = getNearbyReqBody(coord);
-        logger.info(body.toString());
         try {
             return restClient.post()
                     .uri(NEARBY_SEARCH_URL)
+                    .header("X-Goog-FieldMask", "places.name")
                     .body(body)
                     .retrieve()
                     .body(GooglePlaceIdVO.class);
         } catch (HttpClientErrorException e) {
             logError(e);
-            throw new PlaceException(ErrorMessage.GET_PLACE_IMAGE_FAILED);
+            throw new NotFoundException();
         }
     }
 
-    public Map<String, Object> getPlaceReference(Long placeId) {
+    public Map getPlaceReference(Long placeId) {
         List<GooglePlaceIdVO.Place> googlePlaces = getNearbyPlace(placeId).places();
+        logger.info(googlePlaces.toString());
         // 좌표에 대한 장소가 없는 경우
-        if (googlePlaces.isEmpty()) throw new PlaceException(ErrorMessage.GET_PLACE_IMAGE_FAILED);
+        if (googlePlaces.isEmpty()) throw new GoogleApiException("존재하지 않는 장소입니다: " + placeId);
         String idUrl = googlePlaces.getFirst().name();
         String googlePlaceId = idUrl.substring(idUrl.lastIndexOf('/') + 1);
 
 
         try {
             return restClient.get()
-                    .uri(new StringBuilder(PLACE_REFERENCE_URL).append(googlePlaceId).toString())
+                    .uri(PLACE_REFERENCE_URL + "/" + googlePlaceId)
+                    .header("X-Goog-FieldMask", "id,photos")
                     .retrieve()
                     .body(Map.class);
         } catch (HttpClientErrorException e) {
             logError(e);
-            throw new PlaceException(ErrorMessage.GET_PLACE_IMAGE_FAILED);
+            throw new GoogleApiException("장소 정보 획득에 실패했습니다: " + googlePlaceId);
         }
     }
 
@@ -75,7 +76,7 @@ public class PlaceService {
     private PlaceCoordVO getCoordinate(Long placeId) {
         return placeRepository
                 .findCoordById(placeId)
-                .orElseThrow(() -> new PlaceException(ErrorMessage.PLACE_NOT_FOUND));
+                .orElseThrow(() -> new NotFoundException("존재하지 않는 장소입니다: " + placeId));
     }
 
     // Google Places nearby api 요청 데이터

15368e9e2aa64d34794c5b46fb6091a45ca78285
Sin Han <vrdhan212@gmail.com>
2024-08-24 21:49:19 +0900

    update: enable restclient redirect automatically

--- src/main/java/com/kakaotech/back/config/GoogleApiConfig.java
+++ src/main/java/com/kakaotech/back/config/GoogleApiConfig.java
@@ -3,7 +3,12 @@ package com.kakaotech.back.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.net.http.HttpClient;
+import java.time.Duration;
 
 @Configuration
 public class GoogleApiConfig {
@@ -14,8 +19,14 @@ public class GoogleApiConfig {
     @Bean
     RestClient restClient() {
         return RestClient.builder()
-                .defaultHeader("Content-Type", "application/json")
-                .defaultHeader("X-Goog-Api-Key", apiKey)
+                .requestFactory(
+                        new JdkClientHttpRequestFactory(
+                                HttpClient.newBuilder()
+                                        .followRedirects(HttpClient.Redirect.NORMAL)
+                                        .connectTimeout(Duration.ofSeconds(5))
+                                        .build()
+                        )
+                )
                 .build();
     }
 }

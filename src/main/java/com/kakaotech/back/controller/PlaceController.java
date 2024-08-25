638b7567fc5ed7479860142bde9c8d78a1c593bd
Sin Han <vrdhan212@gmail.com>
2024-08-25 23:01:16 +0900

    fix: wrong response body for place image

--- src/main/java/com/kakaotech/back/controller/PlaceController.java
+++ src/main/java/com/kakaotech/back/controller/PlaceController.java
@@ -3,6 +3,7 @@ package com.kakaotech.back.controller;
 import com.kakaotech.back.dto.place.PlaceReqDto;
 import com.kakaotech.back.service.place.PlaceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,8 +16,8 @@ public class PlaceController {
     private final PlaceService placeService;
 
     // TODO: S3 URL 응답
-    @PostMapping("/image")
+    @PostMapping(value = "/image", produces = "image/*")
     public ResponseEntity<byte[]> getPlaceImage(@RequestBody PlaceReqDto dto) {
-        return ResponseEntity.ok(placeService.getPlaceImage(dto.placeId()));
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(placeService.getPlaceImage(dto.placeId()));
     }
 }

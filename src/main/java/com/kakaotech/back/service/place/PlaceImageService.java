373a1dd7a5a44ab54727b108ba4eadd549b3e7d7
Sin Han <vrdhan212@gmail.com>
2024-08-26 10:40:42 +0900

    update: response image when s3 stored

--- src/main/java/com/kakaotech/back/service/place/PlaceImageService.java
+++ src/main/java/com/kakaotech/back/service/place/PlaceImageService.java
@@ -5,9 +5,13 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
 
@@ -24,6 +28,27 @@ public class PlaceImageService {
 
     @Value("${aws.s3.bucket-name}")
     String bucketName;
+
+    // S3에 있는 이미지 파일 가져오기
+    public byte[] fetchImage(Long placeId) {
+        String keyName = "trippop-image/" + placeId + ".jpg";
+
+        try {
+            GetObjectRequest request = GetObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(keyName)
+                    .build();
+
+            ResponseInputStream<GetObjectResponse> response = s3Client.getObject(request);
+            return response.readAllBytes();
+        } catch (S3Exception e) {
+            logger.error("FAILED TO FETCH IMAGE {}: {}", keyName, e.getMessage());
+        } catch (IOException e) {
+            logger.error("FAILED TO READ IMAGE {}: {}", keyName, e.getMessage());
+        }
+        // 에러 발생시 null 반환
+        return null;
+    }
 
     // S3에 업로드
     public void saveImage(Long placeId, byte[] imageData) {
--- src/main/java/com/kakaotech/back/service/place/PlaceService.java
+++ src/main/java/com/kakaotech/back/service/place/PlaceService.java
@@ -5,6 +5,8 @@ import com.kakaotech.back.vo.PlaceCoordVO;
 import com.kakaotech.back.repository.PlaceRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -16,12 +18,19 @@ public class PlaceService {
     private final PlaceRepository placeRepository;
     private final GooglePlaceService googlePlaceService;
     private final PlaceImageService imageService;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public byte[] getPlaceImage(Long placeId) {
+        // S3에 존재하는지 확인
+        byte[] imageData = imageService.fetchImage(placeId);
+        if (imageData != null) return imageData;
+
+        // Google Place API
         String googlePlaceId = getGooglePlaceId(placeId);
         String photoName = getPlacePhotoName(googlePlaceId);
 
-        byte[] imageData = googlePlaceService.getPlaceImage(photoName);
+        // 응답받은 이미지 파일을 S3에 업로드 후 반환
+        imageData = googlePlaceService.getPlaceImage(photoName);
         imageService.saveImage(placeId, imageData);
 
         return imageData;

5cfd57db6bb1efa870f4e038b4a3ca8cab0d9b6e
Sin Han <vrdhan212@gmail.com>
2024-08-26 10:27:24 +0900

    update: upload google place image to s3

--- src/main/java/com/kakaotech/back/service/place/PlaceImageService.java
+++ src/main/java/com/kakaotech/back/service/place/PlaceImageService.java
@@ -1,9 +1,15 @@
 package com.kakaotech.back.service.place;
 
 import com.kakaotech.back.common.exception.GoogleApiException;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,18 +17,26 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Service
+@RequiredArgsConstructor
 public class PlaceImageService {
+    private final S3Client s3Client;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    // TODO: DB에 저장
-    public void saveImage(Long placeId, byte[] imageData) {
+    @Value("${aws.s3.bucket-name}")
+    String bucketName;
 
-        Path path = Paths.get("src", "main", "resources", "image", placeId + ".jpg");
+    // S3에 업로드
+    public void saveImage(Long placeId, byte[] imageData) {
+        String keyName = "trippop-image/" + placeId + ".jpg";
         try {
-            Files.write(path, imageData);
-        } catch (IOException e) {
-            logger.error("FAILED TO SAVE IMAGE: {}", e.getMessage());
-            throw new GoogleApiException("구글 이미지 저장 실패");
+            PutObjectRequest request = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(keyName)
+                    .build();
+            s3Client.putObject(request, RequestBody.fromBytes(imageData));
+        } catch (S3Exception e) {
+            logger.error("FAILED TO UPLOAD IMAGE: {}", e.getMessage());
         }
+
     }
 }

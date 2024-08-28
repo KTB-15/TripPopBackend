05e25bab90c5099d868946cc34ea6c4dc5de7dc7
teddy.kim <qazkyj0310@gmail.com>
2024-08-28 13:03:02 +0900

    add: builder

--- src/main/java/com/kakaotech/back/dto/member/MemberRequestDto.java
+++ src/main/java/com/kakaotech/back/dto/member/MemberRequestDto.java
@@ -5,6 +5,7 @@ import lombok.*;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class MemberRequestDto {
     private String memberId;
     private String password;

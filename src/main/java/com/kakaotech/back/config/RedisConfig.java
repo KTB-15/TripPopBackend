5323133c54857e3e142b03de7a2cdf617fcf841f
teddy.kim <qazkyj0310@gmail.com>
2024-08-30 11:23:03 +0900

    add: redis template

--- src/main/java/com/kakaotech/back/config/RedisConfig.java
+++ src/main/java/com/kakaotech/back/config/RedisConfig.java
@@ -2,10 +2,27 @@ package com.kakaotech.back.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import redis.clients.jedis.JedisPool;
 
 @Configuration
 public class RedisConfig {
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
 
     @Bean
     public JedisPool jedisPoolConnection() {

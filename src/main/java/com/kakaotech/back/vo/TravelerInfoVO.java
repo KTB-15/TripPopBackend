f1fe484d9122ede194dc96aa445a19e2bb8d3d90
Sin Han <vrdhan212@gmail.com>
2024-09-02 16:55:34 +0900

    update: travel info vo fetch

--- src/main/java/com/kakaotech/back/vo/TravelerInfoVO.java
+++ src/main/java/com/kakaotech/back/vo/TravelerInfoVO.java
@@ -1,5 +1,6 @@
 package com.kakaotech.back.vo;
 
+import com.kakaotech.back.entity.Member;
 import lombok.Builder;
 
 // AI에 전송할 요청 본문
@@ -17,4 +18,18 @@ public record TravelerInfoVO(
         String sidoName,
         String sggName
 ) {
+    public static TravelerInfoVO from(Member member) {
+        return TravelerInfoVO.builder()
+                .gender(member.getGender().name())
+                .travelStyle1(member.getTravelStyle1())
+                .travelStyle2(member.getTravelStyle2())
+                .travelStyle4(member.getTravelStyle4())
+                .travelStyle5(member.getTravelStyle5())
+                .travelStyle6(member.getTravelStyle6())
+                .travelStyle7(member.getTravelStyle7())
+                .travelStyle8(member.getTravelStyle8())
+                .sidoName(member.getTravelLikeSGG().getSidoName())
+                .sggName(member.getTravelLikeSGG().getSggName())
+                .build();
+    }
 }

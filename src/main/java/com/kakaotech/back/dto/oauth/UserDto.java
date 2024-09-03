59598ff8a5f2ef3c503aa8027e7739525e287835
teddy.kim <qazkyj0310@gmail.com>
2024-09-03 10:35:50 +0900

    refactor: string to set

--- src/main/java/com/kakaotech/back/dto/oauth/UserDto.java
+++ src/main/java/com/kakaotech/back/dto/oauth/UserDto.java
@@ -1,12 +1,15 @@
 package com.kakaotech.back.dto.oauth;
 
+import com.kakaotech.back.entity.Authority;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.Set;
 
 @Getter
 @Builder
 public class UserDto {
-    private String role;
+    private Set<Authority> authorities;
     private String name;
     private String username;
 }

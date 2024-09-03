950adaca3973d328741f737ff2bc97ed7c9b4d79
teddy.kim <qazkyj0310@gmail.com>
2024-09-03 10:49:36 +0900

    add: save member after oauth login

--- src/main/java/com/kakaotech/back/service/CustomOAuth2UserService.java
+++ src/main/java/com/kakaotech/back/service/CustomOAuth2UserService.java
@@ -5,14 +5,23 @@ import com.kakaotech.back.dto.oauth.CustomOAuth2User;
 import com.kakaotech.back.dto.oauth.GoogleResponse;
 import com.kakaotech.back.dto.oauth.OAuth2Response;
 import com.kakaotech.back.dto.oauth.UserDto;
+import com.kakaotech.back.entity.Authority;
+import com.kakaotech.back.entity.Member;
+import com.kakaotech.back.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    private MemberRepository memberRepository;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -28,7 +37,6 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             //oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
         }
         else if (registrationId.equals("google")) {
-
             oAuth2Response = (OAuth2Response) new GoogleResponse(oAuth2User.getAttributes());
         }
         else if (registrationId.equals("kakao")){
@@ -38,13 +46,39 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             return null;
         }
 
-        //추후 작성
-        String username = oAuth2Response.getProvider()+ " " + oAuth2Response.getProviderId();
-        UserDto userDto = UserDto.builder()
-                .name(oAuth2Response.getName())
-                .username(username)
-                .role("ROLE_USER")
-                .build();
+        String memberId = oAuth2Response.getProvider()+ " " + oAuth2Response.getProviderId();
+        Optional<Member> existData = memberRepository.findOneWithAuthoritiesByMemberId(memberId);
+
+        UserDto userDto = null;
+        if (existData.isEmpty()) {
+            Authority authority = Authority.builder()
+                    .authorityName("ROLE_USER")
+                    .build();
+            Member member = null;
+
+            if(oAuth2Response instanceof GoogleResponse){
+                member = Member.builder()
+                        .memberId(memberId)
+                        .build();
+
+                userDto = UserDto.builder()
+                        .name(oAuth2Response.getName())
+                        .memberId(memberId)
+                        .authorities(Collections.singleton(authority))
+                        .build();
+            }
+
+            memberRepository.save(member);
+        }
+        else {
+            Member existingMember = existData.get();
+
+            userDto = UserDto.builder()
+                    .name(oAuth2Response.getName())
+                    .memberId(memberId)
+                    .authorities(existingMember.getAuthorities())
+                    .build();
+        }
 
         return new CustomOAuth2User(userDto);
     }

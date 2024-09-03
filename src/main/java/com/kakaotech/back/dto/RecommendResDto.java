37fdf62001ef246c177d85a4b540ae66b9608db1
Sin Han <vrdhan212@gmail.com>
2024-09-03 10:38:01 +0900

    update: recommended place info

--- src/main/java/com/kakaotech/back/dto/RecommendResDto.java
+++ src/main/java/com/kakaotech/back/dto/RecommendResDto.java
@@ -1,7 +1,23 @@
 package com.kakaotech.back.dto;
 
+import com.kakaotech.back.entity.Place;
 import lombok.Builder;
 
 @Builder
-public record RecommendResDto (){
+public record RecommendResDto (
+        Long placeId,
+        String areaName,
+        String roadName,
+        byte[] placeImage,
+        Boolean isFavourite
+){
+    public static RecommendResDto of(Place place, byte[] placeImage, Boolean isFavourite){
+        return RecommendResDto.builder()
+                .placeId(place.getId())
+                .areaName(place.getAreaName())
+                .roadName(place.getRoadName())
+                .placeImage(placeImage)
+                .isFavourite(isFavourite)
+                .build();
+    }
 }

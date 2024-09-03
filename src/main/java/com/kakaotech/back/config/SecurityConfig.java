ea9e037e99aff9e2d1de732d79bd26770df610cb
teddy.kim <qazkyj0310@gmail.com>
2024-09-03 09:19:15 +0900

    add: oauth2 config

--- src/main/java/com/kakaotech/back/config/SecurityConfig.java
+++ src/main/java/com/kakaotech/back/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.kakaotech.back.config;
 
 import com.kakaotech.back.jwt.*;
+import com.kakaotech.back.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,6 +27,7 @@ public class SecurityConfig {
     private final TokenProvider tokenProvider;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -35,6 +38,9 @@ public class SecurityConfig {
                         .accessDeniedHandler(jwtAccessDeniedHandler)
                         .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 )
+                .oauth2Login((oauth2) -> oauth2
+                        .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+                                .userService(customOAuth2UserService)))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/member/exists/**", "/member/join", "/auth/login", "/auth/refresh-token").permitAll() // 권한 확인 없는 url
                         .anyRequest().authenticated() // 그 외의 요청은 인증 필요

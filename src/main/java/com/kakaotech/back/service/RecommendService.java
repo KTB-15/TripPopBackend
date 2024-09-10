5b68c4121ed652329b0f1a7eb24751ee2c118d87
Sin Han <vrdhan212@gmail.com>
2024-09-10 13:59:17 +0900

    update: fetch recommendation place from ai server

--- src/main/java/com/kakaotech/back/service/RecommendService.java
+++ src/main/java/com/kakaotech/back/service/RecommendService.java
@@ -1,8 +1,11 @@
 package com.kakaotech.back.service;
 
 
+import com.kakaotech.back.common.exception.GoogleApiException;
 import com.kakaotech.back.common.exception.NotFoundException;
 import com.kakaotech.back.dto.RecommendResDto;
+import com.kakaotech.back.dto.recommendation.RecReqDto;
+import com.kakaotech.back.dto.recommendation.RecResDto;
 import com.kakaotech.back.entity.Favourite;
 import com.kakaotech.back.entity.Member;
 import com.kakaotech.back.entity.Place;
@@ -10,12 +13,16 @@ import com.kakaotech.back.repository.FavouriteRepository;
 import com.kakaotech.back.repository.MemberRepository;
 import com.kakaotech.back.service.place.PlaceService;
 import com.kakaotech.back.util.SecurityUtil;
+import com.kakaotech.back.vo.PlaceCoordVO;
 import com.kakaotech.back.vo.TravelerInfoVO;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClient;
 
 import java.util.*;
 
@@ -27,6 +34,10 @@ public class RecommendService {
     private final PlaceService placeService;
     private final MemberRepository memberRepository;
     private final FavouriteRepository favouriteRepository;
+    private final RestClient restClient;
+
+    @Value("${ai.server.url}")
+    private String AI_SERVER_URL;
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -36,8 +47,8 @@ public class RecommendService {
         Member member = memberRepository.findOneWithAuthoritiesByMemberId(memberId).orElseThrow(
                 () -> new NotFoundException(NotFoundException.messageWithInfo(memberId))
         );
-        TravelerInfoVO travelerInfo = TravelerInfoVO.from(member);
-        List<Place> recommended = getRecommendedPlaces(travelerInfo);
+        RecReqDto recReqDto = RecReqDto.from(member);
+        List<Place> recommended = getRecommendedPlaces(recReqDto);
 
         // 추천받은 여행지에 대한 정보, 이미지 및 즐겨찾기 상태 여부 조회
         List<Long> placeIds = recommended.stream().map(Place::getId).toList();
@@ -50,25 +61,44 @@ public class RecommendService {
 
         // 응답 준비
         ArrayList<RecommendResDto> result = new ArrayList<>();
-        for (int i = 0; i < placeIds.size(); i += 1){
+        for (int i = 0; i < placeIds.size(); i += 1) {
             result.add(RecommendResDto.of(recommended.get(i), placeImages.get(i), isAlreadyLiked.get(i)));
         }
         return result;
     }
 
-    // TODO: AI 서버로부터 추천 여행지 수신
-    private List<Place> getRecommendedPlaces (TravelerInfoVO travelerInfo){
+    private List<Place> getRecommendedPlaces(TravelerInfoVO travelerInfo) {
         List<Long> placeIds = List.of(50L, 51L, 52L, 53L, 54L, 56L, 57L, 58L, 59L, 60L);
         return placeService.getPlacesInOrder(placeIds);
     }
 
+    private List<Place> getRecommendedPlaces(RecReqDto dto) {
+        RecResDto response = fetchRecommendedPlaces(dto);
+        return placeService.getPlacesByAreaNames(response.recommended_places());
+    }
+
     // 추천받은 여행지를 이미 즐겨찾기 했는지 확인
-    private List<Boolean> isAlreadyFavouriteForPlace(List<Favourite> favourites, List<Long> placeIds){
+    private List<Boolean> isAlreadyFavouriteForPlace(List<Favourite> favourites, List<Long> placeIds) {
         List<Long> likedPlaceIds = favourites.stream()
                 .map(f -> f.getPlace().getId())
                 .toList();
         return placeIds.stream()
                 .map(likedPlaceIds::contains)
                 .toList();
+    }
+
+    public RecResDto fetchRecommendedPlaces(RecReqDto request) {
+        try {
+            RecResDto response =  restClient.post()
+                    .uri(AI_SERVER_URL)
+                    .body(request)
+                    .retrieve()
+                    .body(RecResDto.class);
+            logger.info("RESPONSE FROM AI SERVER: {}", response);
+            return response;
+        } catch (HttpClientErrorException e) {
+            logger.error("FAILED TO GET RECOMMENDATION: {}", e.getResponseBodyAsString());
+            throw new GoogleApiException("추천 실패: " + e.getMessage());
+        }
     }
 }
